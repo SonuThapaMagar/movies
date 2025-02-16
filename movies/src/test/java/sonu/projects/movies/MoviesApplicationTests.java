cad9a5eb836da1b4b67b40fd58fe5b031d93fd30
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-16 15:12:20 +0545

    Initial commit

---
+++ movies/src/test/java/sonu/projects/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package sonu.projects.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
