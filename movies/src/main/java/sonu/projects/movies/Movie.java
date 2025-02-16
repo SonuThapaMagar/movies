8b95993e20d2d8a268392a378fe31f9045b6b3be
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-16 16:05:10 +0545

    Models added

---
+++ movies/src/main/java/sonu/projects/movies/Movie.java
@@ -0,0 +1,26 @@
+package sonu.projects.movies;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+    @DocumentReference // This annotation is used to reference another document in MongoDB
+    private List<Review> reviewIds;
+
+
+}
