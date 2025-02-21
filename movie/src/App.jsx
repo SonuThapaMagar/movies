fa2062a23ef09f020b5a8ae548aecae5bc41c345
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-21 21:49:13 +0545

    Axios config setup

---
+++ movie/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import api from './api/axiosConfig';
+import './App.css';
+
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/movies');
+      
+      console.log(response.data);
+      setMovies(response.data);
+
+    } catch (err) {
+
+      console.log(err);
+    }
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }
+    , []);
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
