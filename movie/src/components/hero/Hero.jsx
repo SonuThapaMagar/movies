d3bb6f428bd32c2995603ce6debb326e8a9b6399
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-23 20:05:51 +0545

    Hero section styled

--- movie/src/components/hero/Hero.jsx
+++ movie/src/components/hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = ({ movies }) => {
           return (
             <Carousel.Item key={movie.id || index}>
               <div className="movie-card-container">
-                <div className="movie-card">
+                <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
                   <div className="movie-detail">
                     <div className="movie-poster">
                       <img
